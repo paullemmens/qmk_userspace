934aa7a209d16e4fe7f7c296a544a48d53346a16
Paul Lemmens <paul.lemmens@gmail.com>
2024-11-28 08:53:07 +0100

    Update old Redox firmware to latest status quo
    Add dedicated colon on Raise for Redox like Kyria
    
    Add volume up/down to the redox keyboard
    
    On the Kyrias this is implemented with the encoder but I don't have that
    on the Redox.
    
    Add caps word enabler in Redox rules
    
    I don't believe this should be necessary but at least now it works.
    
    Clean up Redox rules.mk

--- keyboards/redox/keymaps/paullemmens/rules.mk
+++ keyboards/redox/keymaps/paullemmens/rules.mk
@@ -1,21 +1,16 @@
-CONSOLE_ENABLE = no
-
+# Adjustments specific for the Redox.
 OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-LEADER_ENABLE = no        # Enable the Leader Key feature
-MOUSEKEY_ENABLE = no
-TAP_DANCE_ENABLE = no
-UNICODE_ENABLE = no
 WPM_ENABLE = no
 VELOCIKEY_ENABLE = no
 KEY_LOCK_ENABLE = yes
 
-# Reduces firmware bloat due to changes in qmk toolchain.
-# https://discord.com/channels/440868230475677696/473506116718952450/838845851962703893
-GRAVE_ESC_ENABLE = no
-SPACE_CADET_ENABLE = no
+# FIXME: this should be, I think, set globally in the user space rules.mk
+# but doesn't work without it here.
+CAPS_WORD_ENABLE = yes
 
+# Use Link Time Optimization to squeeze last bit of space because I cannot
+# replace the Redox controller so need to minimize space as much as possible.
 LTO_ENABLE = yes
 
 # Bootloader selection
