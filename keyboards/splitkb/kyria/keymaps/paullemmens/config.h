76f04903d8910644cffa78d26d0323f77fcd5a74
Paul Lemmens <paul.lemmens@gmail.com>
2025-03-19 15:22:10 +0100

    Reduce line length for better readability

--- keyboards/splitkb/kyria/keymaps/paullemmens/config.h
+++ keyboards/splitkb/kyria/keymaps/paullemmens/config.h
@@ -27,9 +27,11 @@
 #endif
 
 #ifdef RGBLIGHT_ENABLE
-  /* #define RGBLIGHT_LIMIT_VAL 190 */
-  #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13} // Orients Kyria LEDs to a circle around both halves.
-  //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19} // Orients Kyria LEDs for a left half-right half columnar progression.
+  #define RGBLIGHT_LIMIT_VAL 190 // Reduce power consumption.
+  // Orients Kyria LEDs to a circle around both halves:
+  #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13}
+  // Orients Kyria LEDs for a left half-right half columnar progression:
+  //#define RBGLIGHT_LED_MAP {9,8,6,7,5,3,2,4,1,0,10,12,13,11,14,16,17,15,18,19}
 #endif
 
 // Encoder settings.
