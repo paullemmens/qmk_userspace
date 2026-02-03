1b76da22eb315cc4fd92a9b594d5f031977ee6d7
nlv19419 <paul.lemmens@philips.com>
2026-02-03 14:16:05 +0100

    Replace regular KC_MINS w/ KC_PMNS
    The latter just has the minus sign and the former also has underscore.
    This is an old proposed change that I am saving for posterity that I
    cannot recall the motivation for: it is only used in the redox keymap.

--- users/paullemmens/paullemmens.h
+++ users/paullemmens/paullemmens.h
@@ -126,6 +126,12 @@ enum userspace_layers {
 #define ______________COLEMAK_MOD_DH_R2____________       KC_M,    KC_N,    KC_E,    KC_I,    KC_O
 #define ______________COLEMAK_MOD_DH_R3____________       KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLASH
 
+#define ________________NUMBER_LEFT________________       KC_1,    KC_2,    KC_3,    KC_4,    KC_5
+#define ________________NUMBER_RIGHT_______________       _______, KC_7,    KC_8,    KC_9,    KC_PMNS
+
+#define _________________FUNC_LEFT_________________       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5
+#define _________________FUNC_RIGHT________________       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_PMNS
+
 #define _________________LOWER_L1__________________       _________________FUNC_LEFT_________________
 #define _________________LOWER_L2__________________       KC_F11,  KC_F12,  EA_LSFT, E_UMLAU, _CIRCFL
 #define _________________LOWER_L3__________________       KC_F10,  _GRAVE_, _ACUTE_, OU_LCTL, EURO
@@ -141,11 +147,6 @@ enum userspace_layers {
 #define _________________RAISE_R1__________________       ________________NUMBER_RIGHT_______________
 #define _________________RAISE_R2__________________       _______, KC_4,    KC_5,    KC_6,    KC_PPLS
 #define _________________RAISE_R3__________________       _______, KC_1,    KC_2,    KC_3,    KC_PSLS
-
-#define ________________NUMBER_LEFT________________       KC_1,    KC_2,    KC_3,    KC_4,    KC_5
-#define ________________NUMBER_RIGHT_______________       _______, KC_7,    KC_8,    KC_9,    KC_MINS
-#define _________________FUNC_LEFT_________________       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5
-#define _________________FUNC_RIGHT________________       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_MINS
 
 #define ___________________BLANK___________________       _______, _______, _______, _______, _______
 
