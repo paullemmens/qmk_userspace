09fa8b578f00eff685a0568df708a3bd9d8d1851
Paul Lemmens <paul@paul-lemmens.nl>
2026-05-05 18:43:40 +0200

    Add Halcyon Kyria to my collection
    Squashed commit of the following:
    
    commit 05907acbf39f279ff8294b1a3458709aa84e11f6
    Author: Paul Lemmens <paul@paul-lemmens.nl>
    Date:   Tue May 5 18:22:46 2026 +0200
    
        Add the Halcyon Kyria to the userspace build list
    
    commit 4fdddd3e44e0b0c76e0eddae126fc2cb1db27959
    Author: Paul Lemmens <paul@paul-lemmens.nl>
    Date:   Tue May 5 18:11:43 2026 +0200
    
        Remove default files that are not needed
    
    commit f8fe4cb65068bdd072386d402cfdfc9db61222c9
    Author: Paul Lemmens <paul@paul-lemmens.nl>
    Date:   Tue May 5 18:20:01 2026 +0200
    
        Enable caps word explicitly
    
        This is the same situation as the shared setup for the other Kyrias:
        caps word is specified in the common rules.mk but is not picked up for
        the Halcyon Kyria (yet). This needs to be fixed at a later stage.
    
    commit d6d6d4fa5fa2fad9bd9f9fe495805b58da384085
    Author: Paul Lemmens <paul@paul-lemmens.nl>
    Date:   Tue May 5 18:17:39 2026 +0200
    
        Copy normal kyria keymap almost completely
    
        This is an emergency measure because I really want to prevent that I am
        duplicating this amount of information. Not quite sure how I should
        accomplish this because I still don't really understand how this should
        be done in the userspace situation.
    
        There now is a link in the Userspace documentation that links to example
        repos that I could consult for inspiration.
    
    commit 3ccd314c9ff23ee4153e124238c4daef370fcd4a
    Author: Paul Lemmens <paul@paul-lemmens.nl>
    Date:   Tue May 5 18:16:29 2026 +0200
    
        Set default RGB lighting to rainbow chevron
    
    commit 159ed661e3922a966434125ec340f818080e5c66
    Author: Paul Lemmens <paul@paul-lemmens.nl>
    Date:   Sun May 3 17:25:49 2026 +0200
    
        Copy more material fromm obbut repo for testing
    
    commit c0c3d28c89fc5146161ab7a24a48a14f116a1752
    Author: Paul Lemmens <paul@paul-lemmens.nl>
    Date:   Sun May 3 14:53:56 2026 +0200
    
        Add material for halcyon kyria

---
+++ keyboards/splitkb/halcyon/kyria/keymaps/paullemmens/rules.mk
@@ -0,0 +1,11 @@
+ENCODER_MAP_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+RGBLIGHT_ENABLE = no
+CAPS_WORD_ENABLE = yes
+
+# This adds module functionality to your keyboard (files found in users/halcyon_modules)
+USER_NAME := halcyon_modules
+
+# Include shared code
+VPATH += $(QMK_USERSPACE)/users/paullemmens
+SRC += paullemmens.c
