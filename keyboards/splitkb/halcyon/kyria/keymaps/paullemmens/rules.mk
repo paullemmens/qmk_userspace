3ccd314c9ff23ee4153e124238c4daef370fcd4a
Paul Lemmens <paul@paul-lemmens.nl>
2026-05-05 18:16:29 +0200

    Set default RGB lighting to rainbow chevron

--- keyboards/splitkb/halcyon/kyria/keymaps/paullemmens/rules.mk
+++ keyboards/splitkb/halcyon/kyria/keymaps/paullemmens/rules.mk
@@ -1,4 +1,6 @@
 ENCODER_MAP_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+RGBLIGHT_ENABLE = no
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
